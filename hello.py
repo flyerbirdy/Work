ce124cd207e4d687530aa79caf7ddcc80cbf991b
shaojian1982@gmail.com <shaojian1982@gmail.com>
2018-03-09 09:03:11 +0800

    ‘init’

--- hello.py
+++ hello.py
@@ -1,10 +1,108 @@
+# -*- coding:utf-8 -*-
+
 from flask import Flask
+from flask import request
+import requests
+from tuling import tulin
+import hashlib
+from time import time
+
+import xml.etree.ElementTree as et
+
+import muba
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return '<h1>曹公公</h1>'
+@app.route('/',methods=['GET','POST'])
+def wechat():
 
-if __name__=='__main__':
-	app.run(host='0.0.0.0',port=5000,debug=True)
+    if request.method == 'GET':
+       
+        token = 'hahaha'
+        data = request.args
+        signature = data.get('signature','')
+        timestamp = data.get('timestamp','')
+        nonce = data.get('nonce','')
+        echostr = data.get('echostr','')
+        list = [token, timestamp, nonce]
+        list.sort()
 
+        s = list[0] + list[1] + list[2]
+              
+        hascode = hashlib.sha1(s.encode('utf-8')).hexdigest()
+
+        if hascode == signature:
+       	 return echostr
+        else:
+       	 return ""
+
+
+    if request.method == 'POST':
+         xmldata = request.data
+         xml_rec = et.fromstring(xmldata)
+
+         ToUserName = xml_rec.find('ToUserName').text
+         fromUser = xml_rec.find('FromUserName').text
+         MsgType = xml_rec.find('MsgType').text
+       #  Content = xml_rec.find('Content').text
+         MsgId = xml_rec.find('MsgId').text
+        # Recognition = xml_rec.find('Recognition').text
+       
+
+
+         if MsgType == "text" and Content == "开":
+           Content = xml_rec.find('Content').text
+            #if Content =="开":
+           requests.get(url="http://led.flyerbirdy.com:9088")
+           return "success"
+          #elif Content =="关":
+          # requests.get(url="http://led.flyerbirdy.com:9088/close")
+          # return "success"
+         else:
+           text_str = '''<xml>
+           <ToUserName><![CDATA[%s]]></ToUserName>
+	   <FromUserName><![CDATA[%s]]></FromUserName>
+           <CreateTime>%d</CreateTime>
+           <MsgType><![CDATA[%s]]></MsgType>
+           <Content><![CDATA[%s]]></Content>
+           </xml>'''
+
+           Content = tulin(Content)
+
+          # with open('/home/work/test.txt', 'w') as f:
+          # 	f.write(MsgType)
+
+         return text_str % (fromUser, ToUserName, int(time()),MsgType,Content)  
+        
+         if MsgType == "voice":
+          Recognition = xml_rec.find('Recognition').text
+          if Recognition =="开":
+           requests.get(url="http://led.flyerbirdy.com:9088")
+           return "success"
+          elif Recognition =="关":
+           requests.get(url="http://led.flyerbirdy.com:9088/close")
+           return "success"
+          else:
+           text_str = '''<xml>
+           <ToUserName><![CDATA[%s]]></ToUserName>
+           <FromUserName><![CDATA[%s]]></FromUserName>
+           <CreateTime>%d</CreateTime>
+           <MsgType><![CDATA[‘text’]]></MsgType>
+           <Content><![CDATA[%s]]></Content>
+           </xml>'''
+
+           Content = tulin(Content)
+
+         #with open('/home/work/test.txt', 'w') as f:
+         #      f.write(Content)
+
+          return text_str % (fromUser, ToUserName, int(time()),Recognition)  
+
+
+
+
+  # return "success"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=80)
